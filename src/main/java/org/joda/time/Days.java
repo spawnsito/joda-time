0b380543c6771bda29febb795fa3dd4a3aecda10
Sergio Revilla <replymusic@gmail.com>
2015-03-09 11:51:30 +0100

    snapshot at Mon Mar  9 10:51:11 CET 2015

--- src/main/java/org/joda/time/Days.java
+++ src/main/java/org/joda/time/Days.java
@@ -497,7 +497,7 @@ public final class Days extends BaseSingleFieldPeriod {
                 result =  new Days(days);
             }
 
-            super.getInstance(days);
+            return result;
         }
     }
 }
