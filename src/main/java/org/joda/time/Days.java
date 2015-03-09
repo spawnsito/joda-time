1ac977e6ef725e99ca5f70324c23c842e95edfc8
Sergio Revilla <replymusic@gmail.com>
2015-03-09 11:40:17 +0100

    snapshot at Mon Mar  9 10:51:11 CET 2015

--- src/main/java/org/joda/time/Days.java
+++ src/main/java/org/joda/time/Days.java
@@ -92,7 +92,7 @@ public final class Days extends BaseSingleFieldPeriod {
         cases.add(Integer.MAX_VALUE, MAX_VALUE);
         cases.add(Integer.MIN_VALUE, MIN_VALUE);
 
-        Object result
+        Object result = cases.getInstance(days);
         if (cases.getInstance(days) == null) {
             return new Days(days);
         }
