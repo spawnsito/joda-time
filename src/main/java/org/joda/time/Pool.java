7dc1b1b78e12b422ed8791be17d369f2e2e438fb
Sergio Revilla <replymusic@gmail.com>
2015-03-09 13:01:27 +0100

    snapshot at Mon Mar  9 12:11:42 CET 2015

--- src/main/java/org/joda/time/Pool.java
+++ src/main/java/org/joda/time/Pool.java
@@ -25,7 +25,7 @@ public class Pool {
     }
 
     private void addMinutes(int numeral, Minutes minute) {
-        instances.put(new Integer(numeral), day);
+        instances.put(new Integer(numeral), minute);
     }
 
 
