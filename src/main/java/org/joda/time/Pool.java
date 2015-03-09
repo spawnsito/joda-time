3b47a63f0eab217f42f7d2b44f37dd4fa7998b36
Sergio Revilla <replymusic@gmail.com>
2015-03-09 12:46:06 +0100

    snapshot at Mon Mar  9 12:11:42 CET 2015

--- src/main/java/org/joda/time/Pool.java
+++ src/main/java/org/joda/time/Pool.java
@@ -41,9 +41,10 @@ public class Pool {
 
         Object result = pool.getInstance(numeral);
 
-        
+
 
         if (result == null) {
+            pool.add(numeral);
             result =  new Days(numeral);
         }
 
