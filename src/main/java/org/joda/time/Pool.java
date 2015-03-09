2f2f1c21162f4c415964dc05626465cda8357dfd
Sergio Revilla <replymusic@gmail.com>
2015-03-09 12:11:36 +0100

    extracted pool

---
+++ src/main/java/org/joda/time/Pool.java
@@ -0,0 +1,27 @@
+package org.joda.time;
+
+import java.util.HashMap;
+
+public class Pool {
+
+    private HashMap<Integer, Object> instances;
+
+    public Pool() {
+        this.instances = new HashMap<Integer, Object>();
+    }
+
+    public void add(int numeral, Days day) {
+        instances.put(new Integer(numeral), day);
+    }
+
+    public Object getInstance(int numeral){
+        Object instance = instances.get(new Integer(numeral));
+
+        if (instance == null) {
+            return null;
+        }
+
+        return instance;
+
+    }
+}
