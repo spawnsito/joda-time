8e29ee8dd5d76c6e7310d05f98def72048344ee5
Sergio Revilla <replymusic@gmail.com>
2015-03-09 12:27:24 +0100

    snapshot at Mon Mar  9 12:11:42 CET 2015

--- src/main/java/org/joda/time/Days.java
+++ src/main/java/org/joda/time/Days.java
@@ -486,6 +486,7 @@ public final class Days extends BaseSingleFieldPeriod {
         @Override
         public Days getInstance(int days) {
             Pool pool = Pool.getInstance();
+            
             Object result = pool.getInstance(days);
 
             if (result == null) {
